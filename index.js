6419df692be01e949a5f47034399b79977c573ec
weizhongtan <me@weizhongtan.com>
2018-10-20 22:36:33 +0100

    Add label watching functionality

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const createScheduler = require('probot-scheduler');
 
+function since(secs) {
+  const ttl = secs * 1000
+  let date = new Date(new Date() - ttl)
+
+  // GitHub won't allow it
+  if (date < new Date(0)) {
+    date = new Date(0)
+  }
+  return date
+}
+
 /**
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
@@ -8,21 +19,53 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!');
 
-  app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
+  const triggers = ['pull_request.opened', 'pull_request.reopened'];
+
+  app.on(triggers, async context => {
     const issueComment = context.issue({ body: 'You opened a PR' });
     await context.github.issues.createComment(issueComment);
-
-    const closePR = context.issue({ state: 'closed' });
-    app.log('doing stuff')
-    return context.github.pullRequests.update(closePR);
   });
+
+  app.on('pull_request.labeled', async context => {
+    const label = context.payload.label.name;
+    if (label === 'self destruct') {
+      const comment = context.issue({ body: 'Your PR will self destruct in 5s' });
+      return context.github.issues.createComment(comment);
+    }
+  })
 
   createScheduler(app, {
     delay: !!process.env.DISABLE_DELAY, // delay is enabled on first run
-    interval: 24 * 60 * 60 * 1000 // 1 day
+    interval: 5000
   })
 
-  app.on('schedule.repository', context => {
+  app.on('schedule.repository', async context => {
+    const owner = 'weizhongtan';
+    const repo = 'test';
     // this event is triggered once every day, with a random delay
+    const timestamp = since(2).toISOString().replace(/\.\d{3}\w$/, '')
+
+    const query = `repo:${owner}/${repo} is:open updated:<${timestamp} label:"self destruct"`
+
+    const params = { q: query, sort: 'updated', order: 'desc', per_page: 10 }
+
+    const staleItems = (await context.github.search.issues(params)).data.items;
+
+    app.log(staleItems)
+
+    await Promise.all(staleItems.map(async issue => {
+      await context.github.issues.createComment({
+        owner,
+        repo,
+        number: issue.number,
+        body: 'This PR is about to self destruct',
+      })
+      await context.github.issues.replaceAllLabels({
+        owner,
+        repo,
+        number: issue.number,
+        labels: ['exploded'],
+      });
+    }))
   })
 }
